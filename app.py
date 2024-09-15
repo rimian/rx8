587553d7a2e23edcf0b751ff2689892aea10c85d
Rimian Perkins <rimian.p@gmail.com>
2024-09-15 15:10:38 +1000

    add a basic obd2 query

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+import obd
+
 
 app = Flask(__name__)
 
+
+# Connect to the OBD port
+connection = obd.OBD()
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    cmd = obd.commands.COOLANT_TEMP
+    response = connection.query(cmd)
+
+    if response.is_null():
+        temp_water_value = "No Data (Car Off)"
+    else:
+        temp_water_value = response.value
+
+    return render_template('index.html', temp_water=temp_water_value)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
