8cd79eb006d1e362daf7f72aea37020c72455fea
Rimian Perkins <rimian.p@gmail.com>
2024-09-18 20:31:28 +1000

    edit the startup scwipt

--- run_kiosk.sh
+++ run_kiosk.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-echo "Starting Venv..." >> ~/rx8/logs/server.log
-echo "Running as user: $(whoami)" >> ~/rx8/logs/server.log
-source /home/pi/.venv/rx8/bin/activate
-echo "Using Python: $(which python)" >> ~/rx8/logs/server.log
-
-echo "Starting the server" >> ~/rx8/logs/server.log
-/home/pi/.venv/rx8/bin/python3 ~/rx8/app.py >> ~/rx8/logs/server.log 2>&1 &
-
-
-# Wait for the server to start
 TIMEOUT=30
-PORT=5000
-URL="http://127.0.0.1:$PORT"
+PORT=5000 # Flask server
+URL=localhost
+URL="http://$URL:$PORT"
 
 
-while ! nc -z localhost $PORT; do
+while ! nc -z $URL $PORT; do
   TIMEOUT=$((TIMEOUT - 1))
 
   if [ $TIMEOUT -le 0 ]; then
