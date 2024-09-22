231b562cde7c9fbbe1b8ad52616ec10361777d20
Rimian Perkins <rimian.p@gmail.com>
2024-09-22 22:00:59 +1000

    clean up

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python -m venv ~/.venv/rx8
-source ~/.venv/rx8/bin/activate
-pip install flask
-git clone https://github.com/rimian/rx8.git .
--- run_kiosk.sh
+++ run_kiosk.sh
@@ -2,4 +2,4 @@
 
 cd /home/pi/rx8 2>&1 &
 source /home/pi/.venv/rx8/bin/activate 2>&1 &
-/home/pi/.venv/rx8/bin/python3 rx8/main.py 2>&1 &
+/home/pi/.venv/rx8/bin/python3 main.py 2>&1 &
