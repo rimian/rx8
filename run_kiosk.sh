3b9d97440ce81485c62a5f41290b0d3cd0185f8d
Rimian Perkins <rimian.p@gmail.com>
2024-09-22 20:31:15 +1000

    fix kiosk script

--- run_kiosk.sh
+++ run_kiosk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-source /home/pi/.venv/rx8/bin/activate
-/home/pi/.venv/rx8/bin/python3 ./app.py 2>&1 &
+cd /home/pi/rx8 2>&1 &
+source /home/pi/.venv/rx8/bin/activate 2>&1 &
+/home/pi/.venv/rx8/bin/python3 rx8/main.py 2>&1 &
