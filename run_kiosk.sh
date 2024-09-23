4b48c0f16c5426bc60c8187b900f518d79227436
Rimian Perkins <rimian.p@gmail.com>
2024-09-23 21:32:28 +1000

    logging

--- run_kiosk.sh
+++ run_kiosk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd /home/pi/rx8 2>&1 &
-source /home/pi/.venv/rx8/bin/activate 2>&1 &
-/home/pi/.venv/rx8/bin/python3 main.py 2>&1 &
+LOG_FILE="/home/pi/rx8/boot.log"
+
+cd /home/pi/rx8 >> "$LOG_FILE" 2>&1 &
+source /home/pi/.venv/rx8/bin/activate >> "$LOG_FILE" 2>&1
+/home/pi/.venv/rx8/bin/python3 main.py >> "$LOG_FILE" 2>&1 &
