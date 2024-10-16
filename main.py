b6f0cfa171c8e966bedf92999004b815decffa6a
Rimian Perkins <rimian.p@gmail.com>
2024-10-16 18:31:39 +1100

    print out water temp

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ bus = can.interface.Bus(channel=can_interface, interface='socketcan')
 
 print("Listening for CAN messages on can0...")
 
+water_temp = 1056
+
 # Read and decode CAN messages in real-time
 try:
     while True:
@@ -19,18 +21,17 @@ try:
         can_id = message.arbitration_id
         data = message.data
 
-        print(can_id)
-
-        # # Try to decode the message using the DBC file
-        # try:
-        #     decoded_message = db.decode_message(can_id, data)
-        #     # Print the decoded message in place without scrolling
-        #     sys.stdout.write(f'\rDecoded CAN Message: {decoded_message}')
-        #     sys.stdout.flush()
-        # except (KeyError, cantools.database.errors.DecodeError) as e:
-        #     # If message can't be decoded, print a placeholder
-        #     sys.stdout.write(f'\rUnknown CAN ID: {can_id:#x}, Data: {data.hex()}')
-        #     sys.stdout.flush()
+        # Try to decode the message using the DBC file
+        try:
+            if (can_id == water_temp):
+                decoded_message = db.decode_message(can_id, data)
+                # Print the decoded message in place without scrolling
+                sys.stdout.write(f'\rDecoded CAN Message: {decoded_message}')
+                sys.stdout.flush()
+        except (KeyError, cantools.database.errors.DecodeError):
+            # If message can't be decoded, print a placeholder
+            sys.stdout.write(f'\rUnknown CAN ID: {can_id:#x}, Data: {data.hex()}')
+            sys.stdout.flush()
 
 except KeyboardInterrupt:
     print("\nStopped listening to CAN bus.")
