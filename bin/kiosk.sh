13bbdf177fe7a919c6d128d0476b10dce9e1d7db
Rimian Perkins <rimian.p@gmail.com>
2024-10-19 23:24:23 +1100

    fix log file path

--- bin/kiosk.sh
+++ bin/kiosk.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+LOG_FILE="/var/log/rx8/boot.log"
+
 # Activate the virtual environment and run Python app
 source /home/pi/.venv/bin/activate >> "$LOG_FILE" 2>&1
 /home/pi/.venv/rx8/bin/python3 /home/pi/rx8/main.py >> "$LOG_FILE" 2>&1 &
+sEv7-Hvfgssd@wp
