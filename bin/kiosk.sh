a5223a5a11ca667b638fc660736d8836ba1affbf
Rimian Perkins <rimian.p@gmail.com>
2024-10-19 23:31:56 +1100

    interpolate python3 command

--- bin/kiosk.sh
+++ bin/kiosk.sh
@@ -4,4 +4,4 @@ LOG_FILE="/var/log/rx8/boot.log"
 
 # Activate the virtual environment and run Python app
 source /home/pi/.venv/bin/activate >> "$LOG_FILE" 2>&1
-/home/pi/.venv/bin/python3 /home/pi/rx8/main.py >> "$LOG_FILE" 2>&1 &
+$(which python3) /home/pi/rx8/main.py >> "$LOG_FILE" 2>&1 &
